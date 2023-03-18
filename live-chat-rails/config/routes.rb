8df85de2c63b61f48fe56dc4b61dd59f05ffcbe2
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2023-03-18 21:28:26 +0900

    修正

---
+++ live-chat-rails/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'auth/registrations'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end

3de0b37ab89fd6e17f8233cb7d2acf30dd59fdc5
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2023-03-19 18:16:51 +0900

    remove: いいね削除実装

--- live-chat-rails/app/controllers/messages_controller.rb
+++ live-chat-rails/app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+
+  before_action :authenticate_user!, only: ["index"]
+
   def index
     messages = Message.all.preload(:user)
     messages_array = messages.map do |message|
--- live-chat-rails/db/schema.rb
+++ live-chat-rails/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_074808) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_19_083349) do
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "message_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id"], name: "index_likes_on_message_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.integer "user_id"
     t.string "content"
